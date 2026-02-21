b75c22e0467cd4e7a5e9e44c56c58de365c7a3ef
Abu Horaira <syedmdabuhoraira@gmail.com>
2026-02-21 15:52:23 +0600

    solved build error

--- app/(employees)/employee/profile/page.tsx
+++ app/(employees)/employee/profile/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+export const dynamic = 'force-dynamic';
+
 import { useCurrentUser } from '@/hooks/use-current-user';
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
